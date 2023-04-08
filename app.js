efcbbe883b11999e226c8dbf820c09df90acb60b
Vu Anh Lam <vuanhlam000@gmail.com>
2023-04-08 20:48:48 +0700

    template engine with pug

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const rootDir = require('./utils/path')
 const adminData = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+/**
+ ** set() method allows us set any value globaly in express application 
+ */
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({ extended: false })) 
 
 /**
  ** Only route start with /admin will go the adminRoute file 
@@ -17,7 +24,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoute)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404.pug')
 })
 
 app.listen(4000)
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {prods: products, pageTitle: 'My Shop'})
 })
 
 module.exports = router
